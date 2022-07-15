1c4e0333cafd95f1bd00b9ae5737e6cbc3b64293
Gao Heng <100184550+foxiegh@users.noreply.github.com>
2022-07-16 02:49:25 +0800

    Created Sound Manager and Added Sound Package
    new manager works with current audio clips

--- FarmDemAnimals/Assets/Scripts/SoundManager.cs
+++ FarmDemAnimals/Assets/Scripts/SoundManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SoundManager : MonoBehaviour
+using UnityEngine.Audio;
+public class SoundManager : SingletonManager<SoundManager> 
 {
     // Start is called before the first frame update
-    void Start()
+    [SerializeField] Sound[] sounds;
+    new void Awake()
     {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+        foreach (Sound s in sounds) {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+         }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        Play("Background");
     }
+
+    public void Play (string name) {
+        Sound sound = null;
+        foreach (Sound s in sounds) {
+            if (s.name == name) {
+                sound = s;
+            }
+        }
+        if (sound == null) {
+            Debug.LogWarning("Sound: "+ name + " not found!");
+            return;
+        }
+        sound.source.Play();
+    }
+
 }
